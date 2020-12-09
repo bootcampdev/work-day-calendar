23921fb9fbf619623b7ecf202d09c93ec18deb40
kihBootCamp <kimberleyheuer@hotmail.com>
2020-12-09 08:30:24 -0600

    capture button to update event click

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,45 @@ var currentDate = new Date();
 var currentHour = ((currentDate.getHours()+11) %12 +1);
 var buttonsView = $('#buttons-view');
 
+var myHrs = {};
+var mySchedule2 = { 
+        "hr12" : "7",
+        "hrs" : [
+        {   
+            "hr24": "1",
+            "event": "vacation"
+        }
+    ]
+}
+var mySchedule = [];
+
+
+
 function setupDay() {
     
-    for (var i=8; i<25; i++) {
+    var hrData = {};
+
+    for (var i=7; i<25; i++) {
 
         var hr12 = ((i+11) %12 +1);
 
         var li = document.createElement("li");
          li.textContent = hr12 + ":00";
-         li.setAttribute("data-index", hr12);
-         li.className = "present";
+         li.setAttribute("data-index", hr12);      
+
+         // assign the background color by the hour of the day
+
+         if (i < currentHour) {
+            li.className = "past";
+         }
+         else if (i > currentHour) {
+             li.className = "future";
+         }
+         else {
+            li.className = "present";
+         }
+         
+
          // var li = $("li");
         // li.text(hr12 + ":00");
         // li.addClass("present");
@@ -34,19 +63,46 @@ function setupDay() {
         var button = document.createElement("button");
         button.textContent = "Update Event";
         button.className = "saveBtn";
-        button.id = "bhr" + hr12;
-        //_button.addClass(".saveBtn");
+        //button.id = "bhr" + hr12;
+        button.id = "bhr" + i;
+        //button.addClass(".saveBtn");
     
         li.appendChild(button);
         _hourList.append(li);
+        //_hourList.append(button);
         //console.log(hr12);
         //$("#hour-list").append(li)
+
+        // setup json object to hold today's events
+
+         hrData = {}
+         hrData["hr"] = i;
+         hrData["event"] = "vac";
+
+         myHrs.hr = i;
+         myHrs.event = "vac"
+         
+         mySchedule.push({"hr12": hrData.hr, "event": hrData.event});
+        
+
+         console.log("my schedule " + mySchedule[i]);
+         console.log(hrData);
     }
 }
 
 
 _hourList.on("click", ".saveBtn", function(event) {
     console.log("here " + $(this).attr("id"));
+
+    var s = $(this).attr("id");
+    var hrstr = s.substring(3);
+
+    // if (hrstr > 12) {
+    //     hrstr = hrstr + 1;
+    // }
+
+    console.log(hrstr);
+    console.log(mySchedule[hrstr]);
 })
 
 setupDay();
