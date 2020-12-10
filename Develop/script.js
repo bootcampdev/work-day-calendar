cc2b65df04d966c0258f3ee108ea6abfac5799e1
kihBootCamp <kimberleyheuer@hotmail.com>
2020-12-09 21:22:18 -0600

    saving and displaying schedule

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,11 @@
 var _hourList = $("#hour-list");
 
 var currentDate = new Date();
+var currentHour24 = currentDate.getHours();
 var currentHour = ((currentDate.getHours()+11) %12 +1);
 var buttonsView = $('#buttons-view');
+
+var liId;
 
 var myHrs = {};
 var mySchedule2 = { 
@@ -17,7 +20,7 @@ var mySchedule2 = {
         }
     ]
 }
-var mySchedule = [];
+var mySchedule = JSON.parse(localStorage.getItem("dailySchedule"));
 
 
 
@@ -30,38 +33,60 @@ function setupDay() {
         var hr12 = ((i+11) %12 +1);
 
         var li = document.createElement("li");
-         li.textContent = hr12 + ":00";
-         li.setAttribute("data-index", hr12);      
 
-         // assign the background color by the hour of the day
+        if (i >11) {
+            li.textContent = hr12 + ":00 PM";
+        }
+        else {
+            li.textContent = hr12 + ":00 AM";
+        }
+ 
+        li.setAttribute("data-index", hr12);      
 
-         if (i < currentHour) {
+        // assign the background color by the hour of the day
+
+        if (i < currentHour24) {
             li.className = "past";
-         }
-         else if (i > currentHour) {
-             li.className = "future";
-         }
+        }
+        else if (i > currentHour24) {
+            li.className = "future";
+        }
          else {
             li.className = "present";
-         }
+        }
          
+         // setup json object to hold today's events
 
-         // var li = $("li");
+         hrData = {}
+         hrData["hr24"] = i;
+         hrData["hr12"] = hr12;
+         hrData["event"] = mySchedule[i-7].event;
+
+        // not working li
+        //----------------
+        // var li = $("li");
         // li.text(hr12 + ":00");
         // li.addClass("present");
     
+        // try another way to add li text
+
         var inputEvent = document.createElement("input");
         inputEvent.type = "text";
         inputEvent.className =  "eventInput";
-        inputEvent.id =  "eventhr" + hr12;
-        
-        
+        inputEvent.id =  "eventhr" + hr12;   
+        inputEvent.value = (hrData["event"]);
+                  
         li.appendChild(inputEvent);
+
+        // not working
+        //------------
+        // liId = "eventhr" + hr12;  
+        // li.append($("<li class='no-bullet'><input type='text' id='kim'/></li>"));
 
         //var button = $('<button>').text("Update Event") //.addClass('saveBtn')
         //console.log(button)
         var button = document.createElement("button");
-        button.textContent = "Update Event";
+        button.textContent = "Update";
         button.className = "saveBtn";
         //button.id = "bhr" + hr12;
         button.id = "bhr" + i;
@@ -73,16 +98,12 @@ function setupDay() {
         //console.log(hr12);
         //$("#hour-list").append(li)
 
-        // setup json object to hold today's events
+       
 
-         hrData = {}
-         hrData["hr"] = i;
-         hrData["event"] = "vac";
-
-         myHrs.hr = i;
-         myHrs.event = "vac"
+        //  myHrs.hr = i;
+        //  myHrs.event = "vac"
          
-         mySchedule.push({"hr12": hrData.hr, "event": hrData.event});
+         mySchedule.push({"hr24": hrData.hr24, "hr12": hrData.hr12, "event": hrData.event});
         
 
          console.log("my schedule " + mySchedule[i]);
@@ -94,15 +115,30 @@ function setupDay() {
 _hourList.on("click", ".saveBtn", function(event) {
     console.log("here " + $(this).attr("id"));
 
+    // button clicked on
     var s = $(this).attr("id");
+
+    // button hr assigned to this button in 24 hr format
     var hrstr = s.substring(3);
 
-    // if (hrstr > 12) {
-    //     hrstr = hrstr + 1;
-    // }
-
+    // the array event location for this hour
+    var event_loc = hrstr - 7;
     console.log(hrstr);
-    console.log(mySchedule[hrstr]);
+    console.log(event_loc);
+
+    hr = mySchedule[event_loc].hr12;
+    var event_input = $("#eventhr" + hr);
+
+    mySchedule[event_loc].event = event_input.val();
+
+    // $("#eventhr" + hr).val("test2");
+    // event_input.val("test");
+
+    //console.log(event_loc);
+    console.log(mySchedule[event_loc]);
+
+    // save schedule changes
+    localStorage.setItem("dailySchedule", JSON.stringify(mySchedule));
 })
 
 setupDay();
